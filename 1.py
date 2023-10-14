4ba19db2351f9b7578e4a7fa541e9abcabae2f31
GonzaloM <gonzalo.1718@gmail.com>
2023-10-13 22:03:49 -0300

    piter avisa de ramas

--- 1.py
+++ 1.py
@@ -6,3 +6,5 @@ print("como baron te doy permiso borra las dos anteriores ")
 print("como baron volvere a restaurar todo y despues ue lo v eas lo elimare todo")
 
 print("baron ya hizo su prueba ahora tu has lo mismo")
+
+print("piter ya lo hizo ..tengo que trabajar en una nueva rama y lesugiero qeu aga lo mismo ")
