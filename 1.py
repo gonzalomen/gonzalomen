e9aca14786b96ad2238535749d5f882102847a30
GonzaloM <gonzalo.1718@gmail.com>
2023-10-13 21:47:30 -0300

    pyter realizo trabajo

--- 1.py
+++ 1.py
@@ -1,8 +1,4 @@
 print("Pyter")
 print("hola soy piter ")
 
-print("hola yo soy el baron")
-
-print("como tu mentat borrare que eres el baron en el siguiente commit para que no se den cuenta de quien eres ")
-
 print("como baron te doy permiso borra las dos anteriores ")
