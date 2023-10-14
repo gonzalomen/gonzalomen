a276d4a31da982be62a6bdde561cafe28273b2fc
19 <gonzalo.171819@gmail.com>
2023-10-13 21:45:11 -0300

    baron comfirmo orden

--- 1.py
+++ 1.py
@@ -4,3 +4,5 @@ print("hola soy piter ")
 print("hola yo soy el baron")
 
 print("como tu mentat borrare que eres el baron en el siguiente commit para que no se den cuenta de quien eres ")
+
+print("como baron te doy permiso borra las dos anteriores ")
