e0eafc4e1536b131f9b98ff57b3b9e211f8f74aa
19 <gonzalo.171819@gmail.com>
2023-10-13 21:49:52 -0300

    baron avisa restauracion

--- 1.py
+++ 1.py
@@ -2,3 +2,5 @@ print("Pyter")
 print("hola soy piter ")
 
 print("como baron te doy permiso borra las dos anteriores ")
+
+print("como baron volvere a restaurar todo y despues ue lo v eas lo elimare todo")
