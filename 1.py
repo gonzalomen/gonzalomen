b2c587308f068730dfea91e25b12b163f109534e
GonzaloM <gonzalo.1718@gmail.com>
2023-10-13 21:42:32 -0300

    pyter avisa

--- 1.py
+++ 1.py
@@ -2,3 +2,5 @@ print("Pyter")
 print("hola soy piter ")
 
 print("hola yo soy el baron")
+
+print("como tu mentat borrare que eres el baron en el siguiente commit para que no se den cuenta de quien eres ")
