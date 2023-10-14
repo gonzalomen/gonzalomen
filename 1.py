36b7e605fe939f1c5dc6db79b50f4f9263180343
19 <gonzalo.171819@gmail.com>
2023-10-13 21:59:09 -0300

    baron realizo test

--- 1.py
+++ 1.py
@@ -4,3 +4,5 @@ print("hola soy piter ")
 print("como baron te doy permiso borra las dos anteriores ")
 
 print("como baron volvere a restaurar todo y despues ue lo v eas lo elimare todo")
+
+print("baron ya hizo su prueba ahora tu has lo mismo")
