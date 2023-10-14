85bde1b6c90ac8aa19b90ba7705e9c8c042b38df
19 <gonzalo.171819@gmail.com>
2023-10-14 00:12:12 -0300

    primer commit en la rama del baron

--- 1.py
+++ 1.py
@@ -1,10 +1,3 @@
-print("Pyter")
-print("hola soy piter ")
+print("esta es la rama del baron")
 
-print("como baron te doy permiso borra las dos anteriores ")
-
-print("como baron volvere a restaurar todo y despues ue lo v eas lo elimare todo")
-
-print("baron ya hizo su prueba ahora tu has lo mismo")
-
-print("piter ya lo hizo ..tengo que trabajar en una nueva rama y lesugiero qeu aga lo mismo ")
+print("primer commit sobre la rama del baron")
